1d20fe12766aa412a6f9ee5ad4c989d870be312c
Brittney Kernan <b.kernan@bigspaceship.com>
2014-04-26 16:36:52 -0400

    first commit

---
+++ app/scripts/main.js
@@ -0,0 +1,15 @@
+/*
+	DB Idea: 
+	- have multiple types onf content
+	   pets
+	   health
+	   and something else i forgot
+	- cache media, json, or urls on server
+	- cron job runs every 5 mins to get new data
+	- media must be under a certain size
+	- media must be served fast, cdn
+	- open 5 new tabs must open different images
+	- images shouldn't be seen more than once
+*/
+
+document.body.style.backgroundImage = 'url(http://37.media.tumblr.com/31f5fab562b967b6a4022b7add95c5bb/tumblr_n4501cHPfL1rq2j05o1_1280.jpg)';
